763c58b5dc4586f59b788653c0cf5cb65f581ea6
Emanuele Di Scala <manueldiscala@gmail.com>
2023-04-19 16:19:10 +0200

    Feat:(picalbum push)

--- Js/main.js
+++ Js/main.js
@@ -1,56 +1,54 @@
-// DICHIARAZIONE VARIABILI
-
 let containerEl = document.getElementById("container");
 let slideElement = document.getElementById("slider");
 let slideElements = document.getElementsByClassName("slide");
-let btnext = document.getElementById("buttonext");
+const btnext = document.getElementById("buttonext");
+const btnback = document.getElementById("buttoback");
 let currentslide = 1;
-let btnback = document.getElementById("buttoback");
-let Pics = []
-// PicOne
-
-let PicOne = document.createElement("img");
-PicOne.src ="./img/01.webp";
-PicOne.className="slide";
-slideElement.append(PicOne);
 
 
+let picalbum = []
+
+picalbum.push('./img/01.webp')
+picalbum.push('./img/02.webp')
+picalbum.push('./img/03.webp')
+picalbum.push('./img/04.webp')
+picalbum.push('./img/05.webp')
 
 
+for (let c=0; c < picalbum.length; c++)
+
+    picalbum = document.createElement("img");
+    Firstpic = picalbum [0]
 
 
+btnext.addEventListener("click", function () {
+
+    if (currentslide < 3);
+
+    for (let c = 0; c < slideEls.length; c++) {
+
+        const slide = slideEls[c];
+
+        if (c == currentslide) {
+            slide.className = "hidden";
+        }
+        else {
+            slide.classlist.add("hidden");
+        }
+    }
+    currentslide++;
+})
 
 
-// btnext.addEventListener("click",function(){
-
-//     if (currentslide < 3);
-
-//     for (let c = 0; c < slideEls.length; c++) {
-        
-//         const slide = slideEls[c];
-        
-//         if (c == currentslide) {
-//             slide.className="hidden";    
-//         }
-//         else {
-//             slide.classlist.add("hidden");
-//         }
-//     }
-//     currentslide++;
-// })
-
-
-// btnback.addEventListener("click",function(){
-//     for (let c = 0; c < slideEls.length; c++) {
-    
-//         const slide = slideEls[c];
-        
-//         if (c == currentslide -1) {
-//             slide.classlist.remove("hidden")    
-//         }
-//         else {
-//             slide.classlist.add("hidden")
-//         }
-//     }
-//     currentslide--;
-// })
+btnback.addEventListener("click", function () {
+    for (let c = 0; c < slideEls.length; c++) {
+        const slide = slideEls[c];
+        if (c == currentslide - 1) {
+            slide.classlist.remove("hidden")
+        }
+        else {
+            slide.classlist.add("hidden")
+        }
+    }
+    currentslide--;
+})
